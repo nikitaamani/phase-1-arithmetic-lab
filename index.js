a0a7900134cdbe9c7d6f85af02b979a4a9dba0fe
nikita <nikita.bosire@student.moringaschool.com>
2024-06-29 14:07:00 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = 5;
+
+const num3 = 20;
+const num4 = 5;
+const mod = num3 / num4;
+
+const max = 20;
